8c52e4a8adc2731ee7ecf9f61cb2457549961ae7
Ikor Jefocur <ikor.jfcr@gmail.com>
2023-10-23 18:07:59 +0300

    dynamic invalid request error class for each status code

--- pybit/exceptions.py
+++ pybit/exceptions.py
@@ -34,7 +34,16 @@ class FailedRequestError(Exception):
         )
 
 
-class InvalidRequestError(Exception):
+class InvalidRequestErrorType(type):
+    def __getitem__(cls, status_code):
+        if status_code not in cls.status_types:
+            class RequestStatusError(cls):
+                pass
+            RequestStatusError.status_code = status_code
+            cls.status_types[status_code] = RequestStatusError
+        return cls.status_types[status_code]
+
+class InvalidRequestError(Exception, metaclass = InvalidRequestErrorType):
     """
     Exception raised for returned Bybit errors.
 
@@ -46,10 +55,15 @@ class InvalidRequestError(Exception):
         resp_headers -- The response headers from API. None, if the request caused an error locally.
     """
 
+    status_types = {}
+    status_code = 0
+
+    def __new__(cls, request, message, status_code, time, resp_headers):
+        return super().__new__(cls[status_code])
+
     def __init__(self, request, message, status_code, time, resp_headers):
         self.request = request
         self.message = message
-        self.status_code = status_code
         self.time = time
         self.resp_headers = resp_headers
         super().__init__(
